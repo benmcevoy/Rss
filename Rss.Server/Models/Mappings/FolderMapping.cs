ef0d77fda86b400c973ae12371e595ef79924b02
benmcevoy <ben.mcevoy@gmail.com>
2013-06-08 11:33:56 +1000

    yeah cool

---
+++ Rss.Server/Models/Mappings/FolderMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Rss.Server.Models.Mappings
+{
+    public class FolderMapping : EntityTypeConfiguration<Folder>
+    {
+        public FolderMapping()
+        {
+            HasKey(x => x.Id);
+
+            HasMany(x => x.Feeds).WithOptional(x => x.Folder).WillCascadeOnDelete(true);
+
+            ToTable("Folder");
+        }
+    }
+}
