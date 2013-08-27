006fabe278dc89932ead739c42e81f8dc4eede3e
benmcevoy <ben.mcevoy@gmail.com>
2013-08-27 10:19:29 +1000

    use cookie for simple authentication instead of session

---
+++ Rss.Server/Filters/SessionAuthorizationFilter.cs
@@ -0,0 +1,17 @@
+﻿using Rss.Server.Services;
+using StructureMap;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rss.Server.Filters
+{
+    public class SessionAuthorizationFilter : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var service = ObjectFactory.GetInstance<CookieService>();
+
+            return service.IsSet(httpContext);
+        }
+    }
+}
