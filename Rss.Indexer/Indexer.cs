4ae1af684cce0ce07355f2924d845eaa65475277
benmcevoy <ben.mcevoy@gmail.com>
2014-03-09 21:03:01 +1100

    building a lucene indexer so i can search rss items

---
+++ Rss.Indexer/Indexer.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Lucene.Net.Index;
+
+namespace Rss.Indexer
+{
+    public class Indexer<T>
+    {
+        private readonly IIndexer<T> _indexConfig;
+        private readonly IndexWriter _indexWriter;
+
+        public Indexer(IIndexer<T> indexConfig)
+        {
+            _indexConfig = indexConfig;
+            _indexWriter = new IndexWriter(_indexConfig.Directory, _indexConfig.Analyzer, IndexWriter.MaxFieldLength.LIMITED);
+        }
+
+        public void Index()
+        {
+            var results = _indexConfig.GetDocumentsToIndex().ToList();
+
+            while (results.Any())
+            {
+                results.ForEach(Index);
+
+                _indexWriter.Commit();
+                _indexConfig.SetDocumentsIndexed(results);
+                
+                results = _indexConfig.GetDocumentsToIndex().ToList();
+            }
+        }
+
+        private void Index(T document)
+        {
+            _indexWriter.AddDocument(document.ToLuceneDocument());
+        }
+    }
+}
---
+++ Rss.Indexer/LuceneFieldAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Lucene.Net.Documents;
+
+namespace Rss.Indexer
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LuceneFieldAttribute : Attribute
+    {
+        public LuceneFieldAttribute(string name, Field.Store store = Field.Store.YES,
+            Field.Index index = Field.Index.ANALYZED, Field.TermVector termVector = Field.TermVector.NO)
+        {
+            Store = store;
+            Index = index;
+            TermVector = termVector;
+            Name = name;
+        }
+
+        public string Name { get; private set; }        
+        
+        public Field.Store Store { get; private set; }
+        
+        public Field.Index Index { get; private set; }
+        
+        public Field.TermVector TermVector { get; private set; }
+    }
+}
