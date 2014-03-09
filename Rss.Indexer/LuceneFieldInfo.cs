3532b0e32dd85b133d516a30cc40be6f2a93b8c9
benmcevoy <ben.mcevoy@gmail.com>
2014-03-09 21:44:14 +1100

    more caching

---
+++ Rss.Indexer/LuceneFieldInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Rss.Indexer
+{
+    public class LuceneFieldInfo
+    {
+        public PropertyInfo PropertyInfo { get; set; }
+
+        public LuceneFieldAttribute LuceneFieldAttribute { get; set; }
+    }
+}
