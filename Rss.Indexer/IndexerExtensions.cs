0831b45318af22ea503052c798cc94001fc8ee47
benmcevoy <ben.mcevoy@gmail.com>
2014-03-11 22:11:58 +1100

    inex updates document instead of continually adding it

--- Rss.Indexer/IndexerExtensions.cs
+++ Rss.Indexer/IndexerExtensions.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using Lucene.Net.Documents;
+using Lucene.Net.Index;
 
 namespace Rss.Indexer
 {
@@ -11,6 +12,9 @@ namespace Rss.Indexer
     {
         private static readonly Dictionary<Type, IEnumerable<LuceneFieldInfo>> FieldCache
             = new Dictionary<Type, IEnumerable<LuceneFieldInfo>>(8);
+
+        private static readonly Dictionary<Type, LuceneDocumentAttribute> DocumentCache
+            = new Dictionary<Type, LuceneDocumentAttribute>(8);
 
         public static void ForEach<T>(this IEnumerable<T> set, Action<T> action)
         {
@@ -23,17 +27,16 @@ namespace Rss.Indexer
         public static Lucene.Net.Documents.Document ToLuceneDocument<T>(this T document)
         {
             var luceneDocument = new Lucene.Net.Documents.Document();
-            var fields = GetLuceneFieldInfos(typeof(T));
+            var fields = document.GetLuceneFieldInfos();
 
-            foreach (var field in fields)
+            fields.ForEach(field =>
             {
-                if (field.LuceneFieldAttribute == null) continue;
-
                 var value = GetStringValue(document, field.PropertyInfo);
 
                 luceneDocument.Add(new Field(field.LuceneFieldAttribute.Name, value,
-                    field.LuceneFieldAttribute.Store, field.LuceneFieldAttribute.Index, field.LuceneFieldAttribute.TermVector));
-            }
+                    field.LuceneFieldAttribute.Store, field.LuceneFieldAttribute.Index,
+                    field.LuceneFieldAttribute.TermVector));
+            });
 
             return luceneDocument;
         }
@@ -48,35 +51,58 @@ namespace Rss.Indexer
         public static T ToResult<T>(this Lucene.Net.Documents.Document document) where T : new()
         {
             var result = new T();
-            var fields = GetLuceneFieldInfos(typeof(T));
+            var fields = result.GetLuceneFieldInfos();
 
-            foreach (var field in fields.Where(field => field.LuceneFieldAttribute != null))
+            fields.ForEach(field =>
             {
                 object value;
 
                 if (TryConvertToType(
-                    document.GetField(field.LuceneFieldAttribute.Name).StringValue, 
-                    field.PropertyInfo.PropertyType, 
+                    document.GetField(field.LuceneFieldAttribute.Name).StringValue,
+                    field.PropertyInfo.PropertyType,
                     out value))
                 {
                     field.PropertyInfo.SetValue(result, value);
                 }
-            }
+            });
 
             return result;
         }
 
-        private static IEnumerable<LuceneFieldInfo> GetLuceneFieldInfos(Type type)
+        public static Term GetLuceneDocumentIdTerm<T>(this T document)
         {
+            var type = typeof(T);
+
+            if (DocumentCache.ContainsKey(type)) return DocumentCache[type].IdTerm;
+
+            var luceneDocumentAttribute = type.GetCustomAttribute<LuceneDocumentAttribute>();
+
+            if (luceneDocumentAttribute == null)
+            {
+                    throw new InvalidOperationException(
+                        string.Format("document of type {0} has no LuceneDocumentAttribute", type.Name));
+            }
+
+            DocumentCache[type] = luceneDocumentAttribute;
+
+            return DocumentCache[type].IdTerm;
+        }
+
+        public static IEnumerable<LuceneFieldInfo> GetLuceneFieldInfos<T>(this T document)
+        {
+            var type = typeof(T);
+
             if (FieldCache.ContainsKey(type)) return FieldCache[type];
 
             var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
 
-            var fields = properties.Select(p => new LuceneFieldInfo
-            {
-                PropertyInfo = p,
-                LuceneFieldAttribute = p.GetCustomAttribute<LuceneFieldAttribute>()
-            });
+            var fields = properties
+                .Select(p => new LuceneFieldInfo
+                    {
+                        PropertyInfo = p,
+                        LuceneFieldAttribute = p.GetCustomAttribute<LuceneFieldAttribute>()
+                    })
+                .Where(f => f.LuceneFieldAttribute != null);
 
             FieldCache[type] = fields;
 
@@ -95,8 +121,7 @@ namespace Rss.Indexer
             {
                 switch (typeName)
                 {
-                    case "String":
-                        return true;
+                    case "String": return true;
 
                     case "Int32":
                         result = Convert.ToInt32(value);
--- Rss.Indexer/LuceneFieldAttribute.cs
+++ Rss.Indexer/LuceneFieldAttribute.cs
@@ -7,12 +7,15 @@ namespace Rss.Indexer
     public class LuceneFieldAttribute : Attribute
     {
         public LuceneFieldAttribute(string name, Field.Store store = Field.Store.YES,
-            Field.Index index = Field.Index.ANALYZED, Field.TermVector termVector = Field.TermVector.NO)
+            Field.Index index = Field.Index.ANALYZED, Field.TermVector termVector = Field.TermVector.NO, 
+            float fuzziness = 0.5f, float boost = 1f)
         {
             Store = store;
             Index = index;
             TermVector = termVector;
             Name = name;
+            Fuzziness = fuzziness;
+            Boost = boost;
         }
 
         public string Name { get; private set; }        
@@ -22,5 +25,9 @@ namespace Rss.Indexer
         public Field.Index Index { get; private set; }
         
         public Field.TermVector TermVector { get; private set; }
+
+        public float Fuzziness { get; set; }
+
+        public float Boost { get; set; }
     }
 }
